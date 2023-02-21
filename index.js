20290554e2ec7c7338f5aa147d5e18dd215e5239
Sagaust <austinorge@yahoo.com>
2023-02-21 04:49:29 -0800

    testing some functions that would generate my readme file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const inquirer = require('inquirer');
 
 inquirer.prompt([
@@ -69,3 +70,46 @@ function init() {
 
 // function call to initialize program
 init();
+
+*/
+
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const ejs = require('ejs');
+const marked = require('marked');
+const matter = require('gray-matter');
+const createBadge = require('./createBadge');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+async function createReadme(answers) {
+  // Load the README template file
+  const templateFile = path.join(__dirname, 'templates', 'README.md.ejs');
+  const templateContent = await readFile(templateFile, 'utf8');
+
+  // Render the template with the user's answers
+  const renderedTemplate = ejs.render(templateContent, answers);
+
+  // Convert markdown to HTML for the Table of Contents
+  const tocHtml = marked(renderedTemplate)
+    .replace(/<h(\d) id="(.*?)">(.*?)<\/h\d>/g, (match, level, id, text) => {
+      return `<li><a href="#${id}">${text}</a></li>`;
+    });
+
+  // Add the Table of Contents to the rendered template
+  const finalTemplate = renderedTemplate.replace(/<!--toc-->/, tocHtml);
+
+  // Create a badge for the chosen license
+  const badge = createBadge(answers.license);
+
+  // Add the badge to the top of the README
+  const readmeContent = `${badge}\n\n${finalTemplate}`;
+
+  // Write the README to the appropriate location in the project directory
+  const readmeFile = path.join(__dirname, 'README.md');
+  await writeFile(readmeFile, readmeContent);
+
+  console.log('README.md file created successfully!');
+}
